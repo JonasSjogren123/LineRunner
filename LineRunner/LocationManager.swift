bb2515d495d3df45c2506770da19c7f19d7cc48a
Jonas Sjögrenj <jonas.sjogren@iths.se>
2023-02-26 13:04:02 +0100

    Testprints

--- LineRunner/LocationManager.swift
+++ LineRunner/LocationManager.swift
@@ -74,14 +74,20 @@ class LocationManager : NSObject, CLLocationManagerDelegate , ObservableObject{
                         let latitude = fromDbCoordinate.lat as? Double
                         let longitude = fromDbCoordinate.long as? Double
                         toLmCoordinate = CLLocationCoordinate2D(latitude: latitude ?? 0.0, longitude: longitude ?? 0.0)
-                        print("fromDB:  \(toLmCoordinate)")
+                        print("fromDB toLmCoordinate:  \(toLmCoordinate)")
+                        print("Here there should be a toLmCoordinate")
                         //toLmCoordinates.append(toLmCordinate ?? CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0))
                     case .failure(let error) :
                         print("Error decoding item: \(error)")
                     }
+                    print("1")
                 }
+                print("2")
             }
+            print("3")
         }
+        print("4")
+        if toLmCoordinate == nil {print("toLmCoordinate is nil")} else {print("toLmCoordinate is not nil")}
         return toLmCoordinate ?? CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
     }
 }
