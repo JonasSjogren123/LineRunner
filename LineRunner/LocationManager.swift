d37197153a3d17d0d0d25ccfb4811dbcc772ca06
Jonas Sjögrenj <jonas.sjogren@iths.se>
2023-03-02 16:34:29 +0100

    Coordinates in correct order but needs to delete old coordinates

--- LineRunner/LocationManager.swift
+++ LineRunner/LocationManager.swift
@@ -13,15 +13,15 @@ class LocationManager : NSObject, CLLocationManagerDelegate , ObservableObject{
     let db = Firestore.firestore()
     let manager = CLLocationManager()
     var location : CLLocationCoordinate2D?
-
+    
     @Published var lastCoordinate: CLLocationCoordinate2D?
     @Published var positions = Positions()
     @Published var toLmCoordinate: CLLocationCoordinate2D?
     @Published var toLmCoordinates: [CLLocationCoordinate2D]?
     @Published var lineCoordinates: [CLLocationCoordinate2D] = []
     let defaultCoordinate = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
-
-
+    
+    
     override init() {
         super.init()
         manager.delegate = self
@@ -29,49 +29,46 @@ class LocationManager : NSObject, CLLocationManagerDelegate , ObservableObject{
     
     func startLocationUpdates() {
         manager.requestWhenInUseAuthorization()
+        manager.distanceFilter = 25
         manager.startUpdatingLocation()
+        listenForCoordinateFromFirestore()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         location = locations.first?.coordinate
         
         print("func locationManager Plats uppdaterad! \(location ?? defaultCoordinate)")
-       
+        
         if let toDbLatitude = location?.latitude {
-            print("location?.latitude = \(toDbLatitude)")
             if let toDbLongitude = location?.longitude {
-                print("location?.longitude = \(toDbLongitude)")
-                let toDbPosition = Position(id: "", positionLatitude: toDbLatitude, positionLongitude: toDbLongitude)
-                print("toDbCoordinate = \(toDbPosition)")
+                let toDbPosition = Position(id: "", positionLatitude: toDbLatitude, positionLongitude: toDbLongitude, timeStamp: Date().timeIntervalSince1970)
                 sendPositionToFirestore(position: toDbPosition)
             }
         }
-        lineCoordinates.append(listenForCoordinateFromFirestore())
-        print("lineCoordinates.append(listenForCoordinateFromFirestore())!!")
     }
-
+    
     func sendPositionToFirestore(position: Position) {
         print("func sendCoordinateToFirestore")
         let db = Firestore.firestore()
         let ref = db.collection("Coordinates")
+        // Delete every document in ref
         do {
-           _ = try  ref.addDocument(from: position)
+            _ = try  ref.addDocument(from: position)
             print("try func sendPositionToFirestore \(position)")
         } catch {
             print("error")
         }
-        print("func sendPostitionToFirestore")
-     }
+    }
     
-    func listenForCoordinateFromFirestore() -> CLLocationCoordinate2D {
+    func listenForCoordinateFromFirestore() {
         print("func listenForCoordinateFromFirestore")
-        db.collection("Coordinates").addSnapshotListener {
+        
+        db.collection("Coordinates").order(by: "timeStamp", descending: false).addSnapshotListener {
             snapshot, err in
             guard let snapshot = snapshot else {return}
             if let err = err {
                 print("Error getting document \(err)")
             } else {
-                print("a")
                 self.lineCoordinates.removeAll()
                 for document in snapshot.documents {
                     let result = Result {
@@ -82,24 +79,12 @@ class LocationManager : NSObject, CLLocationManagerDelegate , ObservableObject{
                         let latitude = fromDbCoordinate.positionLatitude
                         let longitude = fromDbCoordinate.positionLongitude
                         self.toLmCoordinate = CLLocationCoordinate2D(latitude: latitude , longitude: longitude )
-                        print("Here there should be a toLmCoordinate")
-                        print("fromDB toLmCoordinate:  \(self.toLmCoordinate ?? self.defaultCoordinate)")
                         self.lineCoordinates.append(self.toLmCoordinate ?? self.defaultCoordinate)
-                        print("lineCoordinates are \(self.lineCoordinates)")
                     case .failure(let error) :
                         print("Error decoding item: \(error)")
                     }
-                    print("1")
                 }
-                print("2")
             }
-            print("3")
         }
-        print("4")
-        if toLmCoordinate == nil {print("toLmCoordinate is nil")} else {
-            print("toLmCoordinate is not nil")
-            print("toLmCoordinate \(toLmCoordinate)")}
-        return toLmCoordinate ?? CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
     }
 }
-
